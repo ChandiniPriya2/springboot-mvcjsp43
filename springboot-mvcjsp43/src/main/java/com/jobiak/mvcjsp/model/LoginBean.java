13a48baf77f7ab185dd9bd1573f7088949b3dbf3
Chandini@22 <Chandinipriya20@gmail.com>
2022-05-17 10:45:59 +0530

    jspwithspring

---
+++ springboot-mvcjsp43/src/main/java/com/jobiak/mvcjsp/model/LoginBean.java
@@ -0,0 +1,22 @@
+package com.jobiak.mvcjsp.model;
+
+public class LoginBean {
+private String userName;
+private String password;
+public LoginBean() {
+	// TODO Auto-generated constructor stub
+}
+public String getUserName() {
+	return userName;
+}
+public void setUserName(String userName) {
+	this.userName = userName;
+}
+public String getPassword() {
+	return password;
+}
+public void setPassword(String password) {
+	this.password = password;
+}
+
+}
