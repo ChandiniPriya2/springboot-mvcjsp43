76467a23d148d1027619778f8976a42d9f807a4e
Chandini@22 <Chandinipriya20@gmail.com>
2022-05-18 09:14:34 +0530

    mvc

--- springboot-mvcjsp43/src/main/java/com/jobiak/mvcjsp/SpringbootMvcjsp43Application.java
+++ springboot-mvcjsp43/src/main/java/com/jobiak/mvcjsp/SpringbootMvcjsp43Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootMvcjsp42Application {
+public class SpringbootMvcjsp43Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootMvcjsp42Application.class, args);
+		SpringApplication.run(SpringbootMvcjsp43Application.class, args);
 	}
 
 }
--- springboot-mvcjsp43/src/test/java/com/jobiak/mvcjsp/SpringbootMvcjsp43ApplicationTests.java
+++ springboot-mvcjsp43/src/test/java/com/jobiak/mvcjsp/SpringbootMvcjsp43ApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringbootMvcjsp42ApplicationTests {
+class SpringbootMvcjsp43ApplicationTests {
 
 	@Test
 	void contextLoads() {
